b666a173224ffffb1e45ed6b3f4daec714618d61
trejitus <sticklesjb@gmail.com>
2012-04-05 15:36:21 -0400

    added back in mongo, login signup logout functioning.

--- app.rb
+++ app.rb
@@ -8,22 +8,22 @@ require "sinatra-authentication"
 #if you change things to do with the app and it's running then it won't be in effect, dur
 set :sinatra_authentication_view_path, Pathname(__FILE__).dirname.expand_path + "views/"
 
-  # use Rack::Session::Cookie, :secret => 'bippity boppity boo, 1 533 whyohyou'
-  # 
-  # Mongoid.configure do |config|
-  #   if ENV['MONGOLAB_URI']
-  #     conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
-  #     uri = URI.parse(ENV['MONGOLAB_URI'])
-  #     config.master = conn.db(uri.path.gsub(/^\//, ''))
-  #   else
-  #     config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('test')
-  #   end
-  # end
-
-  get '/' do
-    haml :index
+use Rack::Session::Cookie, :secret => 'bippity boppity boo, 1 533 whyohyou'
+  
+Mongoid.configure do |config|
+  if ENV['MONGOLAB_URI']
+    conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+    uri = URI.parse(ENV['MONGOLAB_URI'])
+    config.master = conn.db(uri.path.gsub(/^\//, ''))
+  else
+    config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('test')
   end
+end
 
-  get '/application.js' do
-    coffee :application
-  end
+get '/' do
+  haml :index
+end
+
+get '/application.js' do
+  coffee :application
+end
