97b0a729c295688cfa4f14f83abe4753de1b9b22
trejitus <sticklesjb@gmail.com>
2012-04-05 08:03:00 -0400

    added mongolab and mongo stuff with sinatra-auth

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source :rubygems
 gem 'sinatra', '1.1.0'
+gem 'sinatra-authentication', '0.4.1'
 gem 'thin', '1.2.7'
 gem 'coffee-script', '2.2.0'
 gem 'haml', '3.1.4'
 gem 'foreman', '0.39.0'
+gem 'mongoid', '2.4.7'
+gem 'bson_ext', '1.6.1'
--- app.rb
+++ app.rb
@@ -2,6 +2,20 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 require 'coffee-script'
+require 'mongoid'
+require "sinatra-authentication"
+
+use Rack::Session::Cookie, :secret => 'bippity boppity boo, 1 533 whyohyou'
+
+Mongoid.configure do |config|
+  if ENV['MONGOLAB_URI']
+    conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+    uri = URI.parse(ENV['MONGOLAB_URI'])
+    config.master = conn.db(uri.path.gsub(/^\//, ''))
+  else
+    config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('test')
+  end
+end
 
 get '/' do
   haml :index
